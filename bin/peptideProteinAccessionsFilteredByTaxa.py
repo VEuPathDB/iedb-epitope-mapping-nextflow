bd79619fcd81b45077793c1272437070f59ba2b8
John Brestelli <jbrestell@gmail.com>
2024-12-06 16:30:55 -0500

    write peptide fasta file here as we are filtering by taxa

--- bin/peptideProteinAccessionsFilteredByTaxa.py
+++ bin/peptideProteinAccessionsFilteredByTaxa.py
@@ -6,6 +6,7 @@ import sys
 file = sys.argv[1]
 taxonFile = sys.argv[2]
 outputFile = sys.argv[3]
+peptideOutputFastaFile = sys.argv[4]
 
 peptideTabFh = open(file)
 taxonFH = open(taxonFile)
@@ -16,6 +17,9 @@ for line in taxonFH:
   keepTaxa.append(int(line.strip()))
 
 outPut = open(outputFile, 'w')
+pepOutputFasta = open(peptideOutputFastaFile, 'w')
+
+print("##Accessions File", file=outPut)
 
 for line in peptideTabFh:
     if not line.strip():
@@ -24,9 +28,10 @@ for line in peptideTabFh:
     splitLine = line.split("\t")
     iedbTaxId = int(splitLine[2])
 
-    accessionNumber = splitLine[0].split('.')[0]
+    accessionNumber = splitLine[0]
     if iedbTaxId in keepTaxa:
       print(accessionNumber, file=outPut)
+      print(f">{splitLine[1]}\n{splitLine[3]}", file=pepOutputFasta)
 
 peptideTabFh.close()
 outPut.close()
