99a3074aa1e8662f21da66f8d3a9d90a5c405646
John Brestelli <jbrestell@gmail.com>
2024-12-06 14:44:19 -0500

    rm fetchtaxa template

--- modules/templates/fetchTaxa.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-esearch -db taxonomy \
-        -query "txid${taxonID}[Subtree]" \
-        | efetch -format xml | xtract \
-        -pattern TaxaSet -block "*/Taxon" -tab "\n" \
-        -element TaxId > TaxaList.txt
